8fc3ee5fd6f088e542e6c95fe411cebea69ea331
LoicVCT <loic.vancalsteren@outlook.be>
2026-05-03 17:52:15 +0200

    implémentation stack web

---
+++ web/PMUL2/pmul2-team01-app/routes/api.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.post('/scan', async function(req, res) {
+
+});
+
+module.exports = router;
---
+++ web/PMUL2/pmul2-team01-app/routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+
+const { PrismaClient } = require("../generated/prisma");
+
+require("dotenv").config();
+const { PrismaMariaDb } = require ("@prisma/adapter-mariadb");
+const adapter = new PrismaMariaDb({
+  host: "192.168.1.167",
+  user: "team01",
+  password: "team01-thebestone",
+  database: "team01-database",
+  //pas oublier de changer le port plustard 3307-> 3306
+  port: 3307,
+  connectionLimit: 5,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+router.get('/', function(req, res, next) {
+  res.render('homepage', { title: 'Homepage' });
+});
+
+router.get('/orders', function(req, res, next) {
+  res.render('orders', { title: 'Orders' });
+});
+
+router.get('/items', async (req, res) => {
+    const items = await prisma.ITEM.findMany({
+    include: {
+      READ_CYCLES: true,
+      SELECTION_HISTORY: true,
+    },
+  });
+
+    res.render('items', { title: 'Items', items});
+});
+
+module.exports = router;
