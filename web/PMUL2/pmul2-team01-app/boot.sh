39cf06c0ac7cab5d1d3648ef8899b485c934e2d4
JeSaisPas <86721219+JeSaisPaz@users.noreply.github.com>
2026-05-17 14:51:45 +0200

    boot.sh: accept-data-loss for prisma db push

--- web/PMUL2/pmul2-team01-app/boot.sh
+++ web/PMUL2/pmul2-team01-app/boot.sh
@@ -2,7 +2,7 @@
 # Initialisation de la base de donnees et demarrage du serveur
 npm i
 npx prisma generate
-npx prisma db push
+npx prisma db push --accept-data-loss
 
 echo "Demarrage du serveur..."
 npm run dev
