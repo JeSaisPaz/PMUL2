c234bda4862f69faba70fad9c171146048cd7c3a
JeSaisPas <86721219+JeSaisPaz@users.noreply.github.com>
2026-05-19 15:23:22 +0200

    yac

--- AI/TestSuite/test_camera.py
+++ AI/TestSuite/test_camera.py
@@ -26,10 +26,10 @@ time.sleep(0.5)
 # from deleting the blue spectrum. We lock the Red and Blue channels manually.
 cam.set_controls({
     "AwbEnable": False,
-    "ColourGains": (1.3, 1.7)  # (Red Gain, Blue Gain)
+    "ColourGains": (1.3, 1.7)
     "ExposureTime": 9000,
     "AnalogueGain": 1.0,
-    "Saturation": 0.9
+    "Saturation": 0.9    
 })
 
 # Let the auto-exposure settle with our locked color settings
