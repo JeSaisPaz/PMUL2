a5a8b649a57d0a71589be1c464eb3896ad6cb11d
JeSaisPas <86721219+JeSaisPaz@users.noreply.github.com>
2026-05-21 12:27:50 +0200

    Update pmul2-colors.h

--- arduino/pmul2-lib/src/pmul2-colors.h
+++ arduino/pmul2-lib/src/pmul2-colors.h
@@ -4,8 +4,8 @@
 #include <stdint.h>
 
 // byte IDs des couleurs (miroir du backend)
-const uint8_t COLOR_YELLOW  = 0x01;
-const uint8_t COLOR_BLUE    = 0x02;
+const uint8_t COLOR_BLUE  = 0x01;
+const uint8_t COLOR_YELLOW    = 0x02;
 const uint8_t COLOR_MAGENTA = 0x03;
 const uint8_t COLOR_BROWN   = 0x04;
 const uint8_t COLOR_ORANGE  = 0x05;
