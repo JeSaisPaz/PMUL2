dbe76deeaf639d51cc9bde831efe3d324d1d765d
LoicVCT <loic.vancalsteren@outlook.be>
2026-05-23 18:19:20 +0200

    fix

--- debugAtHome/web/PMUL2/pmul2-team01-app/routes/api.js
+++ debugAtHome/web/PMUL2/pmul2-team01-app/routes/api.js
@@ -19,7 +19,7 @@ module.exports = function (io) {
             //const code = error.status || error.code || 500;
 
             console.error("API error : " + error.message)
-            res.status(500).json({ error: error.message }); // ← actually respond
+            res.status(res.code).json({ error: error.message }); // ← actually respond
         }
     };
 
